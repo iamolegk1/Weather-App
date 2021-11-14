eee8c8353910b6d8e4bab8023c98a594d6ce0ff7
iamolegk1 <oleg.kropivka5@gmail.com>
2021-11-14 16:54:59 +0200

    Fix import

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Stack from "@mui/material/Stack";
 import LoadingButton from "@mui/lab/LoadingButton";
 import Button from "@mui/material/Button";
